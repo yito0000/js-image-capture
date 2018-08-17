0ffefb2eaf429cf17b827c5952c674c7dbd69a92
yito0000 <dummy@email>
2018-08-17 15:05:49 +0900

    ボタンクリックしてスナップショットが表示されるように実装

---
+++ main.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', function(){
+    var player = document.getElementById('player'); 
+    var snapshotCanvas = document.getElementById('snapshot');
+    var captureButton = document.getElementById('capture');
+  
+    var handleSuccess = function(stream) {
+      // Attach the video stream to the video element and autoplay.
+      player.srcObject = stream;
+    };
+  
+    captureButton.addEventListener('click', function() {
+      var context = snapshot.getContext('2d');
+      // Draw the video frame to the canvas.
+      context.drawImage(player, 0, 0, snapshotCanvas.width, 
+          snapshotCanvas.height);
+    });
+  
+    navigator.mediaDevices.getUserMedia({video: true})
+        .then(handleSuccess);
+});
